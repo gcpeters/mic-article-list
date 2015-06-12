5e39c0cd7f2213761dab9c9c4e8be4b70da61b56
Grant Caesar Peters <grant@learnvest.com>
2015-06-12 14:21:33 -0400

    Updated list view with load more articles button and click listener

--- src/jsx/List.jsx
+++ src/jsx/List.jsx
@@ -4,7 +4,8 @@
 
 var React = require('React'),
 	moment = require('moment'),
-	_ = require('lodash');
+	_ = require('lodash'),
+	$ = require('jquery');
 
 function articleMapper (article, index) {
 
@@ -33,76 +34,71 @@ function articleMapper (article, index) {
 	);
 }
 
-function fetch (ops) {
-    var xmlhttp = new XMLHttpRequest(),
-    	noop = function () {};
-
-    ops = _.extend({
-    	url: 'data/articles.json',
-    	success: noop,
-    	error: noop
-    }, ops);
-
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState === XMLHttpRequest.DONE ) {
-
-           if (xmlhttp.status === 200){
-
-           		try {
-
-           			ops.success(JSON.parse(xmlhttp.responseText));
-           		} catch (err) {
-
-           			ops.error(err);
-           		}
-           } else if (xmlhttp.status === 400) {
-
-              ops.error('There was an error 400');
-           } else {
-               ops.error('something else other than 200 was returned');
-           }
-        }
-    };
-
-    xmlhttp.open('GET', ops.url, true);
-    xmlhttp.send();
-}
-
 module.exports = React.createClass({
 	fetchArticles: function (url) {
-		fetch({
+		$.ajax({
 			url: url,
+			dataType: 'json',
+			cache: false,
 			success: this.onArticlesReady,
 
-			error: function (xhr, status, err) {
+			error: function(xhr, status, err) {
 				console.error && console.error(url, status, err.toString());
 			}
 		});
 	},
 
 	onArticlesReady: function (articles) {
+		var state = this.state,
+			start = 10;;
+
+		if (_.isArray(state.articles)) {
+
+			start += state.articles.length;
+			articles = state.articles.concat(articles);
+		}
+
 		this.setState({
+			start: 0,
+			stop: start,
 			articles: articles
 		});
 	},
 
 	render: function () {
-		var articleNodes = _.map(this.state.articles, articleMapper);
+		var state = this.state,
+			articles = state.articles.slice(state.start, state.stop),
+			articleNodes = _.map(articles, articleMapper);
 
 		return (
 			<div className="mic-article-list">
 				{articleNodes}
+				<a href="#" className="mic-load-more-articles">Load More Articles</a>
 			</div>
 		);
 	},
 
 	getInitialState: function() {
 		return {
+			start: 0,
+			stop: 0,
 			articles: []
 		};
 	},
 
+	loadMoreArticles: function (evt) {
+		var state = this.state;
+
+		this.setState({
+			stop: this.state.stop + 10
+		});
+
+		evt.preventDefault();
+	},
+
 	componentDidMount: function() {
+		$('.mic-load-more-articles').on('click', this.loadMoreArticles);
+
 		this.fetchArticles('data/articles.json');
 	}
 });
